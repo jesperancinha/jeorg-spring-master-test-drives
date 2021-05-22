70c6b2624770b606d693a91110d4feb1363fdb1f
Joao Esperancinha <jofisaes@gmail.com>
2021-05-22 13:00:48 +0200

    🎖 jeorg-spring-master-5-test-drives - car-parts - 🏎 - Random Port

--- car-parts/src/test/java/org/jesperancinha/smtd/carparts/CarPartsLauncherTest.java
+++ car-parts/src/test/java/org/jesperancinha/smtd/carparts/CarPartsLauncherTest.java
@@ -3,7 +3,9 @@ package org.jesperancinha.smtd.carparts;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class CarPartsLauncherTest {
 
     @Test
