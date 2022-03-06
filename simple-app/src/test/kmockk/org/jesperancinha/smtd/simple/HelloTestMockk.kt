c30651f838509bd63b4d34c755341c2becde7d15
Joao Esperancinha <jofisaes@gmail.com>
2022-03-06 10:35:20 +0100

    🌿 Still not working

--- simple-app/src/test/kmockk/org/jesperancinha/smtd/simple/HelloTestMockk.kt
+++ simple-app/src/test/kmockk/org/jesperancinha/smtd/simple/HelloTestMockk.kt
@@ -1,34 +1,28 @@
 package org.jesperancinha.smtd.simple
 
-import io.kotest.core.spec.style.WordSpec
-import io.kotest.core.test.TestCase
+import io.kotest.core.spec.style.StringSpec
 import io.kotest.extensions.spring.SpringExtension
 import io.kotest.matchers.shouldNotBe
-import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.MockK
 import org.jesperancinha.smtd.simple.service.Service
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 
+@SpringBootTest
+@AutoConfigureWebTestClient
 @ContextConfiguration(classes = [(Service::class)])
-class HelloTestMockk : WordSpec() {
+class HelloTestMockk : StringSpec() {
 
     override fun extensions() = listOf(SpringExtension)
 
-    @MockK
+    @MockK(relaxed = true)
     lateinit var service: Service
 
-    override fun beforeAny(testCase: TestCase) {
-        MockKAnnotations.init(this, relaxUnitFun = true)
-        super.beforeAny(testCase)
-    }
-
     init {
-        "SpringExtension" should {
-            "have autowired the service" {
-                service shouldNotBe null
-            }
+        "should have autowired the service" {
+            service shouldNotBe null
         }
     }
 }
-
 
--- simple-app/src/test/kotlin/org/jesperancinha/smtd/simple/HelloTest.kt
+++ simple-app/src/test/kotlin/org/jesperancinha/smtd/simple/HelloTest.kt
@@ -4,8 +4,12 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.extensions.spring.SpringExtension
 import io.kotest.matchers.shouldNotBe
 import org.jesperancinha.smtd.simple.service.Service
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 
+@SpringBootTest
+@AutoConfigureWebTestClient
 @ContextConfiguration(classes = [(Service::class)])
 class HelloTest(service: Service) : WordSpec() {
 
@@ -19,5 +23,3 @@ class HelloTest(service: Service) : WordSpec() {
         }
     }
 }
-
-
