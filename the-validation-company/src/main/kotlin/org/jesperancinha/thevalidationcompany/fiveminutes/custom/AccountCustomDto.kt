168a372c10ab8e306cf8ee7356d8303a08125a47
jesperancinha <jofisaes@gmail.com>
2023-11-13 12:05:23 +0100

    Adds validation examples

---
+++ the-validation-company/src/main/kotlin/org/jesperancinha/thevalidationcompany/fiveminutes/custom/AccountCustomDto.kt
@@ -0,0 +1,42 @@
+package org.jesperancinha.thevalidationcompany.fiveminutes.custom
+
+import jakarta.validation.Constraint
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+import jakarta.validation.Payload
+import org.springframework.beans.BeanWrapperImpl
+import org.springframework.stereotype.Component
+import kotlin.reflect.KClass
+
+@AtLeastOne(fields = ["postAddress", "street"])
+data class AccountCustomDto(
+    val postAddress: String,
+    val street: String?,
+    val houseNumber: Long?,
+    val postCode: String
+)
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
+@Constraint(validatedBy = [OneAtLeastNotNullConstraintValidator::class])
+annotation class AtLeastOne(
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = [],
+    val message: String = "Object instance of violates one not null constraint",
+    val fields: Array<String> = []
+)
+
+@Component
+class OneAtLeastNotNullConstraintValidator : ConstraintValidator<AtLeastOne, Any> {
+    private lateinit var fields: Array<String>
+    override fun initialize(configuration: AtLeastOne) {
+        fields = configuration.fields
+    }
+
+    override fun isValid(obj: Any, context: ConstraintValidatorContext): Boolean {
+        val beanWrapper = BeanWrapperImpl(obj)
+        return fields
+            .mapNotNull { propertyName -> beanWrapper.getPropertyValue(propertyName) }
+            .count() >= 1L
+    }
+}
---
+++ the-validation-company/src/main/kotlin/org/jesperancinha/thevalidationcompany/rest/FiveMinutesController.java
@@ -0,0 +1,9 @@
+package org.jesperancinha.thevalidationcompany.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("5minutes")
+public class FiveMinutesController {
+}
