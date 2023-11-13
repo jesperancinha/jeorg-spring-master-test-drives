fb0868bcd174a53e60b116f8da165bdbb18a51dd
jesperancinha <jofisaes@gmail.com>
2023-11-13 11:47:16 +0100

    The vault updated

--- the-vault/src/main/kotlin/org/jesperancinha/smtd/the/vault/configuration/TheVaultSecurityAdapter.kt
+++ the-vault/src/main/kotlin/org/jesperancinha/smtd/the/vault/configuration/TheVaultSecurityAdapter.kt
@@ -3,6 +3,7 @@ package org.jesperancinha.smtd.the.vault.configuration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
 
@@ -10,15 +11,16 @@ import org.springframework.security.web.SecurityFilterChain
 open class TheVaultSecurityAdapter {
 
     @Bean
-    open fun filterChain(http: HttpSecurity): SecurityFilterChain = http.authorizeRequests()
-        ?.requestMatchers(HttpMethod.GET, "/vault/silver/**")
-        ?.hasAuthority("KEEPER")
-        ?.requestMatchers(HttpMethod.GET, "/vault/silver/**")
-        ?.hasRole("KEEPER")
-        ?.requestMatchers(HttpMethod.GET, "/vault/silver/test")
-        ?.permitAll()
-        ?.and()
-        ?.httpBasic()
-        ?.and()
-        ?.formLogin()?.and()?.build() ?: throw RuntimeException("Failed to configure security!")
+    open fun filterChain(http: HttpSecurity): SecurityFilterChain =
+        http.authorizeHttpRequests {
+            it.requestMatchers(HttpMethod.GET, "/vault/silver/**")
+                .hasAuthority("KEEPER")
+                .requestMatchers(HttpMethod.GET, "/vault/silver/**")
+                .hasRole("KEEPER")
+                .requestMatchers(HttpMethod.GET, "/vault/silver/test")
+                .permitAll()
+        }
+            .httpBasic(Customizer.withDefaults())
+            .formLogin(Customizer.withDefaults())
+            .build()
 }
