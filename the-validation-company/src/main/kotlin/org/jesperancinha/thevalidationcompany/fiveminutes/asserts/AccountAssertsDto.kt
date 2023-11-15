808650b6d452efa9ea96939f1195d6f6175b7046
Joao Esperancinha <jofisaes@gmail.com>
2023-11-15 14:00:03 +0100

    Extra tests for JSON ignore

--- the-validation-company/src/main/kotlin/org/jesperancinha/thevalidationcompany/fiveminutes/asserts/AccountAssertsDto.kt
+++ the-validation-company/src/main/kotlin/org/jesperancinha/thevalidationcompany/fiveminutes/asserts/AccountAssertsDto.kt
@@ -1,16 +1,19 @@
 package org.jesperancinha.thevalidationcompany.fiveminutes.asserts
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.validation.constraints.AssertTrue
 
 data class AccountAssertsDto(
     val postAddress: String?,
     val street: String?,
     val houseNumber: Long?,
-    val postCode: String
+    val postCode: String,
 ) {
+    @JsonIgnore
     @AssertTrue(message = "Or a street or a post address")
     fun isStreetOrPostAddress() = street != null || postAddress != null
 
+    @JsonIgnore
     @AssertTrue(message = "Or street and house number or both null")
     fun isStreetAndHouseNumberOrNull() = street != null && houseNumber != null || street == null && houseNumber == null
 }
--- the-validation-company/src/test/kotlin/org/jesperancinha/thevalidationcompany/rest/fiveminutes/FiveMinutesControllerAssertsTest.kt
+++ the-validation-company/src/test/kotlin/org/jesperancinha/thevalidationcompany/rest/fiveminutes/FiveMinutesControllerAssertsTest.kt
@@ -1,6 +1,10 @@
 package org.jesperancinha.thevalidationcompany.rest.fiveminutes
 
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.kotest.matchers.equality.shouldBeEqualToComparingFields
+import io.kotest.matchers.equality.shouldBeEqualUsingFields
+import io.kotest.matchers.equals.shouldBeEqual
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.string.shouldContain
@@ -9,7 +13,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.MediaType
+import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -22,6 +26,7 @@ class FiveMinutesControllerAssertsTest @Autowired constructor(
 ) {
 
     val objectMapper by lazy { ObjectMapper() }
+    var typeRef: TypeReference<HashMap<String, Any>> = object : TypeReference<HashMap<String, Any>>() {}
 
     @Test
     fun `should post asserts request and fail when neither street or postAddress are filled in`() {
@@ -38,62 +43,84 @@ class FiveMinutesControllerAssertsTest @Autowired constructor(
                         postCode = "1234AA"
                     )
                 )
-            ).contentType(MediaType.APPLICATION_JSON)
+            ).contentType(APPLICATION_JSON)
         )
             .andExpect(status().is4xxClientError)
             .andReturn()
-            .run { response.errorMessage }
-            .run { this shouldContain "Invalid request content" }
+            .response.errorMessage
+            .shouldNotBeNull() shouldContain "Invalid request content"
 
     }
 
     @Test
     fun `should post asserts request and success with street and houseNumber`() {
+        val objectString = objectMapper.writeValueAsString(
+            AccountAssertsDto(
+                postAddress = null,
+                houseNumber = 10,
+                street = "Up the street",
+                postCode = "1234AA"
+            )
+        )
         mockMvc.perform(
             post(
                 "/5minutes/asserts",
 
                 ).content(
-                objectMapper.writeValueAsString(
-                    AccountAssertsDto(
-                        postAddress = null,
-                        houseNumber = 10,
-                        street = "Up the street",
-                        postCode = "1234AA"
-                    )
-                )
-            ).contentType(MediaType.APPLICATION_JSON)
+                objectString
+            ).contentType(APPLICATION_JSON)
         )
 
             .andExpect(status().isOk)
             .andReturn()
-            .run { response.contentAsString }
-            .run { shouldNotBeNull() }
+            .response.contentAsString
+            .shouldNotBeNull()
+            .let {
+                val expected = objectMapper
+                    .readValue(objectString, typeRef)
+                    .apply {
+                        remove("streetAndHouseNumberOrNull")
+                        remove("streetOrPostAddress")
+                    }
+                objectMapper.readValue(it, typeRef)
+                    .shouldBeEqual(expected)}
 
     }
 
     @Test
     fun `should post asserts request and success with street only and no house number`() {
+        val accountAssertsDto = AccountAssertsDto(
+            postAddress = null,
+            houseNumber = 1,
+            street = "Up the street",
+            postCode = "1234AA"
+        )
+        val objectString = objectMapper
+            .writeValueAsString(accountAssertsDto)
         mockMvc.perform(
             post(
                 "/5minutes/asserts/programmatic",
 
-                ).content(
-                objectMapper.writeValueAsString(
-                    AccountAssertsDto(
-                        postAddress = null,
-                        houseNumber = 1,
-                        street = "Up the street",
-                        postCode = "1234AA"
-                    )
                 )
-            ).contentType(MediaType.APPLICATION_JSON)
+                .content(
+                    objectString
+                ).contentType(APPLICATION_JSON)
         )
 
             .andExpect(status().isOk)
             .andReturn()
-            .run { response.contentAsString }
-            .run { shouldNotBeNull() }
+            .response.contentAsString
+            .shouldNotBeNull()
+            .let {
+                val expected = objectMapper
+                    .readValue(objectString, typeRef)
+                    .apply {
+                        remove("streetAndHouseNumberOrNull")
+                        remove("streetOrPostAddress")
+                    }
+                objectMapper.readValue(it, typeRef)
+                .shouldBeEqual(expected)}
+
 
     }
 
@@ -112,7 +139,7 @@ class FiveMinutesControllerAssertsTest @Autowired constructor(
                         postCode = "1234AA"
                     )
                 )
-            ).contentType(MediaType.APPLICATION_JSON)
+            ).contentType(APPLICATION_JSON)
         )
             .andExpect(status().is4xxClientError)
             .andReturn()
