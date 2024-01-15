50ed46340e96c7d0d8478e0dd166892e944a4fcc
jesperancinha <jofisaes@gmail.com>
2024-01-15 22:29:18 +0100

    Testing the WebTestClient

--- car-parts-reactive/src/main/kotlin/org/jesperancinha/carpartsreactive/CarPartsReactiveApplication.kt
+++ car-parts-reactive/src/main/kotlin/org/jesperancinha/carpartsreactive/CarPartsReactiveApplication.kt
@@ -1,26 +1,63 @@
 package org.jesperancinha.carpartsreactive
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.reactive.asFlow
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.kotlin.core.publisher.toFlux
+import java.util.*
 import kotlin.coroutines.coroutineContext
 
 @SpringBootApplication
+@EnableR2dbcRepositories
 class CarPartsReactiveApplication
 
 fun main(args: Array<String>) {
-	runApplication<CarPartsReactiveApplication>(*args)
+    runApplication<CarPartsReactiveApplication>(*args)
 }
 
 @RestController
 @RequestMapping
-class CarController(){
+class CarController(
+    val carPartRepository: CarPartRepository
+) {
 
-	@GetMapping("/parts")
-	suspend fun getParts(): List<String> {
-		println(coroutineContext)
-		return listOf("breaks", "keys", "lights")
-	}
+    @GetMapping("/parts")
+    suspend fun getParts(): List<String> {
+        println(coroutineContext)
+        return listOf("breaks", "keys", "lights")
+    }
+    @GetMapping("/parts/correct")
+    fun getPartsCorrect(): Flow<String> {
+        return listOf("breaks", "keys", "lights").asFlow()
+    }
+
+    @GetMapping("/parts/{id}")
+    suspend fun getPartsCorrect(
+        @PathVariable("id") id:String
+    ): CarPart? = carPartRepository.findById(id)
+
+    @PostMapping("/parts")
+    suspend fun createPart(@RequestBody carPart: CarPart) {
+        println(carPartRepository)
+    }
 }
+
+data class CarPart(
+    val id: String,
+    val name: String
+)
+
+interface CarPartRepository : CoroutineCrudRepository<CarPart, String>
--- car-parts-reactive/src/test/kotlin/org/jesperancinha/carpartsreactive/CarPartsReactiveApplicationTests.kt
+++ car-parts-reactive/src/test/kotlin/org/jesperancinha/carpartsreactive/CarPartsReactiveApplicationTests.kt
@@ -1,13 +1,75 @@
 package org.jesperancinha.carpartsreactive
 
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.platform.commons.logging.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
+import org.springframework.test.web.reactive.server.WebTestClient
+import java.util.*
 
 @SpringBootTest
 class CarPartsReactiveApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
+
+}
+
+@SpringJUnitConfig(classes = [PostControllerTest.TestConfig::class])
+class PostControllerTest {
+    @Configuration(proxyBeanMethods = false)
+    @ComponentScan(basePackageClasses = [CarController::class])
+    internal class TestConfig {
+
+        @Bean
+        @Primary
+        fun postRepository(): CarPartRepository = mockk<CarPartRepository>()
+    }
+
+    @Autowired
+    lateinit var carPartRepository: CarPartRepository
+
+    private lateinit var client: WebTestClient
+
+    @BeforeEach
+    fun setup() {
+        client = WebTestClient.bindToController(CarController(carPartRepository))
+            .configureClient()
+            .build()
+    }
+
+    @AfterEach
+    fun teardown() {
+        clearAllMocks()
+    }
+
+    @Test
+    fun `get post by id`() = runTest {
+        val id = UUID.randomUUID().toString()
+        coEvery { carPartRepository.findById(any<String>()) } returns
+                CarPart(
+                    id = id,
+                    name = "wow"
+                )
+        client.get().uri("/parts/first")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody().jsonPath("$.name").isEqualTo("wow")
+
+        coVerify(exactly = 1) { carPartRepository.findById(any()) }
+    }
 
 }
