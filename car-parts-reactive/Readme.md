c21e899d0f0e46bbb310c6352de76ee024776212
jesperancinha <jofisaes@gmail.com>
2024-01-25 00:04:54 +0100

    Adds docs

---
+++ car-parts-reactive/Readme.md
@@ -0,0 +1,20 @@
+# car-parts-reactive 🛠
+
+## Introduction
+
+Study of a reactive application using Kotlin Coroutines
+
+## References
+
+### Books
+
+-   Cosmina, I. (11th December 2019). <i>Pivotal Certified Professional Core Spring 5 Developer Exam: A Study Guide Using Spring Framework 5</i>. (Second Edition). Apress
+-   Sharma, R. (September 2018). <i>Hands-On Reactive Programming with Reactor</i>. (First Edition). Packt
+-   Cosmina, I. Harrop, R. Schaefer, C. Ho, C. (October 2017). <i>Pro Spring 5 An In-Depth Guide to the Spring Framework and Its Tools</i>. (Fifth Edition). Apress
+-   Winch, R. Mularien, P. (December 2012). <i>Spring Security 3.1</i>. (Second Edition). Packt Publishing
+-   Kurniawan, B. Deck, P. (January 2015). <i>Servlet, JSP & Spring MVC</i>. (First Edition). Brainy Software
+-   Long, J. (2020). <i>Reactive Spring</i>. (First Edition). Josh Long
+
+## About me
+
+[![GitHub followers](https://img.shields.io/github/followers/jesperancinha.svg?label=Jesperancinha&style=for-the-badge&logo=github&color=grey "GitHub")](https://github.com/jesperancinha)
--- car-parts-reactive/src/main/kotlin/org/jesperancinha/carpartsreactive/CarPartsReactiveApplication.kt
+++ car-parts-reactive/src/main/kotlin/org/jesperancinha/carpartsreactive/CarPartsReactiveApplication.kt
@@ -2,22 +2,11 @@ package org.jesperancinha.carpartsreactive
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.reactive.asFlow
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
-import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import reactor.kotlin.core.publisher.toFlux
-import java.util.*
+import org.springframework.web.bind.annotation.*
 import kotlin.coroutines.coroutineContext
 
 @SpringBootApplication
