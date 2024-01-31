e3cfe2c664eebcfc9414521f46d78b4d01f3ffdd
jesperancinha <jofisaes@gmail.com>
2024-01-31 08:13:01 +0100

    Creates aggregate function

--- repeated-dislikes/src/main/resources/data.sql
+++ repeated-dislikes/src/main/resources/data.sql
@@ -29,29 +29,6 @@ values (random_uuid(), @userid,
 
 
 
-
-
-
-
-
-insert into FIX.USERS (ID, NAME)
-values (random_uuid(), 'CatOne');
-insert into FIX.SHOPS (ID, NAME)
-values (random_uuid(), 'Cats Para Dice');
-SET @receipt3=random_uuid();
-SET @receipt4=random_uuid();
-insert into FIX.RECEIPTS (ID, USER_ID, SHOP_ID)
-values (@receipt3,
-        (select ID from FIX.USERS WHERE NAME = 'CatOne'),
-        (select ID from FIX.SHOPS WHERE NAME = 'Cats Para Dice')
-       );
-insert into FIX.RECEIPTS (ID, USER_ID, SHOP_ID)
-values (@receipt4,
-        (select ID from FIX.USERS WHERE NAME = 'CatOne'),
-        (select ID from FIX.SHOPS WHERE NAME = 'Cats Para Dice')
-       );
-
-
 truncate table BADFIX.USERS;
 truncate table BADFIX.SHOPS;
 truncate table BADFIX.RECEIPTS;
@@ -90,28 +67,24 @@ truncate table FIX.USERS;
 truncate table FIX.SHOPS;
 truncate table FIX.RECEIPTS;
 truncate table FIX.USERS_RECEIPTS;
-truncate table FIX.USERS_SHOPS;
 insert into FIX.USERS (ID, NAME)
-values (random_uuid(), 'CatOne');
+values (random_uuid(), 'CatOne Fix');
 insert into FIX.SHOPS (ID, NAME)
-values (random_uuid(), 'Cats Para Dice');
+values (random_uuid(), 'Cats Para Dice Fix');
 SET @receipt3=random_uuid();
 SET @receipt4=random_uuid();
-SET @userid=(select ID from FIX.USERS WHERE NAME = 'CatOne');
+SET @userid=(select ID from FIX.USERS WHERE NAME = 'CatOne Fix');
 insert into FIX.RECEIPTS (ID, USER_ID, SHOP_ID)
 values (@receipt3,
-        (select ID from FIX.USERS WHERE NAME = 'CatOne'),
-        (select ID from FIX.SHOPS WHERE NAME = 'Cats Para Dice')
+        (select ID from FIX.USERS WHERE NAME = 'CatOne Fix'),
+        (select ID from FIX.SHOPS WHERE NAME = 'Cats Para Dice Fix')
        );
 insert into FIX.RECEIPTS (ID, USER_ID, SHOP_ID)
 values (@receipt4,
-        (select ID from FIX.USERS WHERE NAME = 'CatOne'),
-        (select ID from FIX.SHOPS WHERE NAME = 'Cats Para Dice')
+        (select ID from FIX.USERS WHERE NAME = 'CatOne Fix'),
+        (select ID from FIX.SHOPS WHERE NAME = 'Cats Para Dice Fix')
        );
-SET @shopId=(select ID from FIX.SHOPS WHERE NAME = 'Cats Para Dice');
 insert into FIX.USERS_RECEIPTS(USER_ID, RECEIPTS_ID)
 values (@userid,@receipt3);
 insert into FIX.USERS_RECEIPTS(USER_ID, RECEIPTS_ID)
 values (@userid,@receipt4);
-insert into FIX.USERS_SHOPS(USER_ID, SHOPS_ID)
-values (@userid,@shopId);
