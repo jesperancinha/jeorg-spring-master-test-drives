2b07fe104aefed03d04e7179b3bc1860c5b13958
jesperancinha <jofisaes@gmail.com>
2024-03-03 12:35:56 +0100

    Should insert a book assertion

--- furniture-k-shop/src/test/kotlin/org/jesperancinha/smtd/furniture/service/CaseServiceAspectJTest.kt
+++ furniture-k-shop/src/test/kotlin/org/jesperancinha/smtd/furniture/service/CaseServiceAspectJTest.kt
@@ -1,5 +1,9 @@
 package org.jesperancinha.smtd.furniture.service
 
+import io.kotest.assertions.any
+import io.kotest.matchers.collections.shouldContainAnyOf
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldHave
 import org.assertj.core.api.Assertions.assertThat
 import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
 import org.jesperancinha.smtd.furniture.model.Case
@@ -19,13 +23,14 @@ open class CaseServiceAspectJTest(
 
     @Test
     fun insertCaseStartNonTransactional() {
+        val case = Case(
+            id = -1L,
+            designation = "Book case",
+            weight = 230
+        )
         try {
             caseService.insertCaseStartOneTransactional(
-                Case(
-                    id = 100L,
-                    designation = "Book case",
-                    weight = 230
-                )
+                case
             )
         } catch (exception: Exception) {
             ConsolerizerComposer.outSpace()
@@ -33,6 +38,8 @@ open class CaseServiceAspectJTest(
                 .reset()
         }
 
-        assertThat(caseService.getAll()).hasSize(1);
+        val all = caseService.getAll()
+        all.shouldContainAnyOf(case)
+        all.filter { it.designation == "Book case" }.shouldHaveSize(1)
     }
 }
