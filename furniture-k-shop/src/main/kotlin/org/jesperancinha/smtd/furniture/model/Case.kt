967c6b567c257fe85b8f5923809561277b4cdaa6
jesperancinha <jofisaes@gmail.com>
2024-03-19 18:19:15 +0100

    Expands annotation example

--- furniture-k-shop/src/main/kotlin/org/jesperancinha/smtd/furniture/model/Case.kt
+++ furniture-k-shop/src/main/kotlin/org/jesperancinha/smtd/furniture/model/Case.kt
@@ -2,14 +2,27 @@ package org.jesperancinha.smtd.furniture.model
 
 import jakarta.persistence.*
 
+/**
+ * Case
+ * Spring plugin is specifically not used in this project!
+ *
+ * @property id
+ * @property designation
+ * @property weight
+ * @constructor Create empty Case
+ */
 @Entity
 @Table(name = "shelf_case")
 data class Case(
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long,
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    val id: Long?,
     var designation: String?,
     var weight: Long?
 ) {
     constructor() : this(0L, null, null)
+
+    override fun toString(): String {
+        return super.toString()
+    }
 }
